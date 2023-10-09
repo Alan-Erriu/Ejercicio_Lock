0ddf05b8c21216b96d21f0a3717b33e6442e1a9b
Alan Erriu <erriu0273@gmail.com>
2023-10-09 16:46:07 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio_Asincronia/Program.cs
@@ -0,0 +1,13 @@
+﻿miArray.Array miArray = new miArray.Array(30000, 45000);
+
+
+Console.WriteLine("del 0 al 15.000 el mayor es" + "= " + miArray.BuscarMayorPrimeraMitad());
+
+Console.WriteLine("del 15.000 al 30.000 el mayor es" + "= " + miArray.BuscarMayorSegundaMitad());
+
+
+
+Console.WriteLine("El mayor es:");
+miArray.BuscarMayorDelArrayCompleto();
+
+
---
+++ miArray/Array.cs
@@ -0,0 +1,89 @@
+﻿namespace miArray
+{
+    public class Array
+    {
+        private int[] _Array;
+        private int _numerosAleatorio;
+        private Random _generadorAleatorio;
+        private object _bloqueo = new object();
+
+        public Array(int tamañoArray, int numeroAleatorio)
+        {
+            _Array = new int[tamañoArray];
+
+            _numerosAleatorio = numeroAleatorio;
+
+            _generadorAleatorio = new Random();
+
+            LlenarArrayConNumerosAleatorios();
+
+        }
+
+        private void LlenarArrayConNumerosAleatorios()
+        {
+            lock (_bloqueo)
+            {
+                for (int i = 0; i < _Array.Length; i++)
+                {
+                    _Array[i] = _generadorAleatorio.Next(0, _numerosAleatorio + 1);
+                }
+            }
+        }
+
+        public int BuscarMayorPrimeraMitad()
+        {
+            lock (_bloqueo)
+            {
+                var primeraMitad = _Array.Length / 2;
+                var mayorEnPrimeraMitad = _Array[0];
+
+                for (var i = 0; i < primeraMitad; i++)
+                {
+                    if (_Array[i] > mayorEnPrimeraMitad)
+                    { mayorEnPrimeraMitad = _Array[i]; }
+
+
+                }
+                return mayorEnPrimeraMitad;
+            }
+        }
+        public int BuscarMayorSegundaMitad()
+        {
+            lock (_bloqueo)
+            {
+                var primeraMitad = _Array.Length / 2;
+                var mayorEnSegundaMitad = _Array[0];
+
+                for (var i = primeraMitad; i < _Array.Length; i++)
+                {
+                    if (_Array[i] > mayorEnSegundaMitad)
+                    { mayorEnSegundaMitad = _Array[i]; }
+
+                }
+
+
+
+                return mayorEnSegundaMitad;
+            }
+        }
+
+        public int BuscarMayorDelArrayCompleto()
+        {
+
+            int mayorPrimeraMitad = BuscarMayorPrimeraMitad();
+            int mayorSegundaMitad = BuscarMayorSegundaMitad();
+
+
+            if (mayorPrimeraMitad < mayorSegundaMitad)
+            {
+                Console.WriteLine(mayorSegundaMitad);
+                return mayorSegundaMitad;
+            }
+            else
+            {
+                Console.WriteLine(mayorPrimeraMitad);
+                return mayorPrimeraMitad;
+            }
+        }
+    }
+}
